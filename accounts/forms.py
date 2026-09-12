a7b09243c394731bafca78bb4e26543a4318dff0
edoardolai <edoardolai17@gmail.com>
2026-09-12 21:13:28 +0200

    student registration form and page

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import User
+
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+    email = forms.EmailField()
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # Teacher accounts are created through the admin.
+        fields = ("username", "first_name", "last_name", "email")
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+def index(request):
+    return render(request, "accounts/index.html")
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("accounts:index")
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = RegistrationForm()
+    return render(request, "accounts/register.html", {"form": form})
