8362a1ecde567abc5c9778ce90f951f2cd49896b
edoardolai <edoardolai17@gmail.com>
2026-09-12 21:14:45 +0200

    login logout and teacher student list

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -7,4 +8,9 @@ app_name = "accounts"
 urlpatterns = [
     path("", views.index, name="index"),
     path("accounts/register/", views.register, name="register"),
+    path("accounts/login/", auth_views.LoginView.as_view(
+        template_name="accounts/login.html",
+    ), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("students/", views.student_list, name="student-list"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render
+from django.views.decorators.http import require_safe
 
 from .forms import RegistrationForm
+from .models import User
 
 
 def index(request):
@@ -14,7 +20,18 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("accounts:index")
+            messages.success(request, "Account created. You can now log in.")
+            return redirect("accounts:login")
     else:
         form = RegistrationForm()
     return render(request, "accounts/register.html", {"form": form})
+
+
+@login_required
+@require_safe
+def student_list(request):
+    if request.user.role != User.Role.TEACHER:
+        raise PermissionDenied
+    students = User.objects.filter(role=User.Role.STUDENT, is_active=True).order_by("username")
+    page = Paginator(students, 25).get_page(request.GET.get("page"))
+    return render(request, "accounts/student_list.html", {"page": page})
